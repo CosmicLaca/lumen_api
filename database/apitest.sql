61b24a4812f9a410beded00ff022bb9a4a0d6a71
Perjés László <perjeslaszlo@gmail.com>
2018-12-17 20:18:03 +0100

    New init database for testing

--- database/apitest.sql
+++ database/apitest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 16, 2018 at 09:33 PM
+-- Generation Time: Dec 17, 2018 at 08:13 PM
 -- Server version: 10.2.19-MariaDB-1:10.2.19+maria~bionic
 -- PHP Version: 7.2.13-1+ubuntu18.04.1+deb.sury.org+1
 
@@ -62,11 +62,11 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `uuid`, `name`, `price`, `currency`, `created_at`, `updated_at`) VALUES
-(1, 'a274b2ae-0171-11e9-9ca7-0235404fedf8', 'product_1', '10', 'HUF', '2018-12-16 20:31:57', '2018-12-16 20:31:57'),
-(2, 'a274b3f6-0171-11e9-9ca7-0235404fedf8', 'product_2', '20', 'HUF', '2018-12-16 20:31:57', '2018-12-16 20:31:57'),
-(3, 'a274b486-0171-11e9-9ca7-0235404fedf8', 'product_3', '30', 'HUF', '2018-12-16 20:31:57', '2018-12-16 20:31:57'),
-(4, 'a274b4d3-0171-11e9-9ca7-0235404fedf8', 'product_4', '40', 'HUF', '2018-12-16 20:31:57', '2018-12-16 20:31:57'),
-(5, 'a274b51b-0171-11e9-9ca7-0235404fedf8', 'product_5', '50', 'HUF', '2018-12-16 20:31:57', '2018-12-16 20:31:57');
+(1, 'e38546f9-022b-11e9-9ccf-0235404fedf8', 'product_1', '10', 'HUF', '2018-12-17 18:45:13', '2018-12-17 18:45:13'),
+(2, 'e3854890-022b-11e9-9ccf-0235404fedf8', 'product_2', '20', 'HUF', '2018-12-17 18:45:13', '2018-12-17 18:45:13'),
+(3, 'e385494a-022b-11e9-9ccf-0235404fedf8', 'product_3', '30', 'HUF', '2018-12-17 18:45:13', '2018-12-17 18:45:13'),
+(4, 'e38549c1-022b-11e9-9ccf-0235404fedf8', 'product_4', '40', 'HUF', '2018-12-17 18:45:13', '2018-12-17 18:45:13'),
+(5, 'e3854a2d-022b-11e9-9ccf-0235404fedf8', 'product_5', '50', 'HUF', '2018-12-17 18:45:13', '2018-12-17 18:45:13');
 
 --
 -- Triggers `products`
@@ -145,9 +145,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `uuid`, `username`, `password`, `name`, `phone`, `email`, `created_at`, `updated_at`) VALUES
-(1, 'a2762227-0171-11e9-9ca7-0235404fedf8', 'User_1', '123456', 'user 1', '06/70 11111-22222', 'name_1@gmail.com', '2018-12-16 20:31:58', '2018-12-16 20:31:58'),
-(2, 'a2762334-0171-11e9-9ca7-0235404fedf8', 'User_2', '123456', 'user 2', '06/70 33333-44444', 'name_2@gmail.com', '2018-12-16 20:31:58', '2018-12-16 20:31:58'),
-(3, 'a27623c2-0171-11e9-9ca7-0235404fedf8', 'User_3', '123456', 'user 3', '06/70 5555-66666', 'name_3@gmail.com', '2018-12-16 20:31:58', '2018-12-16 20:31:58');
+(1, 'e386e720-022b-11e9-9ccf-0235404fedf8', 'User_1', '123456', 'user 1', '06/70 11111-22222', 'name_1@gmail.com', '2018-12-17 18:45:13', '2018-12-17 18:45:13'),
+(2, 'e386e867-022b-11e9-9ccf-0235404fedf8', 'User_2', '123456', 'user 2', '06/70 33333-44444', 'name_2@gmail.com', '2018-12-17 18:45:13', '2018-12-17 18:45:13'),
+(3, 'e386e932-022b-11e9-9ccf-0235404fedf8', 'User_3', '123456', 'user 3', '06/70 5555-66666', 'name_3@gmail.com', '2018-12-17 18:45:13', '2018-12-17 18:45:13');
 
 --
 -- Triggers `users`
@@ -182,19 +182,26 @@ ALTER TABLE `migrations`
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `products_name_unique` (`name`),
+  ADD UNIQUE KEY `products_uuid_unique` (`uuid`);
 
 --
 -- Indexes for table `shoppingcarts`
 --
 ALTER TABLE `shoppingcarts`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `shoppingcarts_uuid_unique` (`uuid`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_username_unique` (`username`),
+  ADD UNIQUE KEY `users_phone_unique` (`phone`),
+  ADD UNIQUE KEY `users_email_unique` (`email`),
+  ADD UNIQUE KEY `users_uuid_unique` (`uuid`);
 
 --
 -- AUTO_INCREMENT for dumped tables
